3321ac3547c0e9df26bb6553c4c4e13acf419458
Zxlove720 <146498837+Zxlove720@users.noreply.github.com>
2023-10-28 17:03:48 +0800

    Add files via upload

---
+++ define/define/初识define.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+/*#define num 100
+int main()
+{
+	printf("%d\n", num);
+	int arr[num] = {0};
+	printf("%d\n", arr);
+	return 0;
+}
+#define add(x,y) ((x)+(y))
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = add(a, b);
+	printf("%d\n", c);
+	return 0;
+}*/
+#define add(x,y) ((x)+(y))
+int main()
+{
+	int a = 0;
+	int b = 20;
+	int c = add(a, b);
+		printf("%d\n", c);
+	return 0;
+}
