308d9b2fc223aace6c111743ac7a6347d8a4cb84
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-01-14 18:22:33 +0800

    Add files via upload
    成绩管理系统的头文件

---
+++ test.h
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+void menu(void);
+void output(int arr[], int n);
+void find(int arr[], int n);
+void sort(int arr[], int n);
+
+
