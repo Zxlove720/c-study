734c89bcac8838a027574fdbab2229d8fb2c9ef9
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-01-15 17:47:20 +0800

    Add files via upload
    第一个c++程序 hello world

---
+++ c++hello_world.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<windows.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world ";
+	cout << "hello world";
+	cout << "%d" << endl;
+	printf("%d", 2);
+	Sleep(2000);
+	system("cls");
+	return 0;
+}
