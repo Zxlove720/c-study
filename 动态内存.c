9744d7d4a11a15225f9c1276ec9f362c01143e5d
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-01-23 18:20:41 +0800

    Add files via upload
    C语言动态内存管理的练习

---
+++ 动态内存.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+//int main()
+//{
+//	int* ram = (int*)malloc(40);
+//	if (ram == NULL)
+//	{
+//		printf("ram:%s\n", strerror(errno));
+//		return 1;
+//	}
+//	int i;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(ram + i) = i + 1;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(ram + i));
+//	}
+//	free(ram);
+//	ram = NULL;
+//	return 0;
+//}
