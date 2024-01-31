7076ba93a2aad87e4de5385054528513eee7a076
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-01-31 18:04:25 +0800

    Add files via upload
    C语言与杨辉三角

---
+++ 杨辉.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() 
+{    
+	int rows, coef = 1, space, i, j;  
+	printf("请输入杨辉三角的行数： ");   
+	scanf("%d", &rows);    
+	for (i = 0; i < rows; i++) 
+	{        
+		for (space = 1; space <= rows - i; space++)
+		{
+			printf("  ");
+		}	
+	}       
+	for (j = 0; j <= i; j++) 
+	{            
+		if (j == 0 || i == 0) 
+		{                
+			coef = 1;            
+		} 
+		else 
+		{                
+			coef = coef * (i - j + 1) / j;          
+		}            
+		printf("%4d", coef);        
+	}        
+	printf("\n");     
+	return 0;
+}
