98fb6794624910811f086b53ac3785599c895c69
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-02-03 18:44:02 +0800

    Add files via upload
    三子棋中如何初始化棋盘

---
+++ 棋盘初始化.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+void intboard(char board[ROW][COL], int row, int col)
+{
+	int i, j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
