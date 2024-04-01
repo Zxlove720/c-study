af37ffa4bed8c49c1ca203fa70f798e3fe9c4c7b
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-04-01 23:31:15 +0800

    Add files via upload
    2024.4.1数据结构复习

--- 2024.4.1.c
+++ 2024.4.1.c
@@ -5,6 +5,238 @@
 
 typedef int datatype;
 
+//typedef struct stack
+//{
+//	datatype data;
+//	struct stack* next;
+//}stack;
+
+//void initstack(stack** top)
+//{
+//	*top = NULL;
+//}
+//
+//bool isempty(stack* top)
+//{
+//	if (top == NULL)
+//	{
+//		return true;
+//	}
+//	return false;
+//}
+//
+//void push(stack** top)
+//{
+//	printf("请输入入栈的元素\n");
+//	datatype element;
+//	scanf("%d", &element);
+//	stack* newnode = (stack*)malloc(sizeof(stack));
+//	if (newnode == NULL)
+//	{
+//		printf("error\n");
+//		return;
+//	}
+//	newnode->data = element;
+//	newnode->next = *top;
+//	*top = newnode;
+//}
+//
+//datatype pop(stack** top)
+//{
+//	if (isempty(*top))
+//	{
+//		printf("is empty\n");
+//		exit(1);
+//	}
+//	datatype poped;
+//	stack* temp;
+//	temp = *top;
+//	poped = temp->data;
+//	*top = temp->next;
+//	return poped;
+//}
+//
+//int main()
+//{
+//	stack* top;
+//	initstack(&top);
+//	bool flag = isempty(top);
+//	if (!flag)
+//	{
+//		printf("error\n");
+//		return -1;
+//	}
+//	printf("多少元素要入栈?\n");
+//	int n;
+//	scanf("%d", &n);
+//	int i;
+//	for (i = 0; i < n; i++)
+//	{
+//		push(&top);
+//	}
+//	printf("要出栈几个元素?\n");
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", pop(&top));
+//	}
+//	return 0;
+//}
+
+
+//void initstack(stack** top)
+//{
+//	*top = NULL;
+//}
+//
+//bool isempty(stack* top)
+//{
+//	if (top == NULL)
+//	{
+//		return true;
+//	}
+//	return false;
+//}
+//
+//void push(stack** top)
+//{
+//	datatype element;
+//	printf("请输入要入栈的元素\n");
+//	scanf("%d", &element);
+//	stack* newnode = (stack*)malloc(sizeof(stack));
+//	if (newnode == NULL)
+//	{
+//		printf("error\n");
+//		return;
+//	}
+//	newnode->data = element;
+//	newnode->next = *top;
+//	*top = newnode;
+//}
+//
+//datatype pop(stack** top)
+//{
+//	if (isempty(*top))
+//	{
+//		printf("error\n");
+//		exit(1);
+//	}
+//	datatype poped;
+//	stack* temp;
+//	temp = *top;
+//	poped = temp->data;
+//	*top = temp->next;
+//	free(temp);
+//	temp = NULL;
+//	return poped;
+//}
+//
+//int main()
+//{
+//	stack* top;
+//	initstack(&top);
+//	bool flag = isempty(top);
+//	if (!flag)
+//	{
+//		printf("error\n");
+//		return -1;
+//	}
+//	int i = 0;
+//	int n;
+//	printf("有多少个元素要入栈\n");
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		push(&top);
+//	}
+//	printf("有多少元素要出栈?\n");
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", pop(&top));
+//	}
+//	return 0;
+//}
+//#define maxsize 100
+//
+//typedef struct queue
+//{
+//	datatype data[maxsize];
+//	int front;
+//	int rear;
+//	int tag;
+//}queue;
+//
+//void initqueue(queue* q)
+//{
+//	q->front = q->rear = 0;
+//	q->tag = 0;
+//}
+//
+//bool isempty(queue* q)
+//{
+//	if (q->front == q->rear && q->tag == 0)
+//	{
+//		return true;
+//	}
+//	return false;
+//}
+//
+//void enqueue(queue* q)
+//{
+//	if (q->front == q->rear && q->tag == 1)
+//	{
+//		printf("full\n");
+//		return;
+//	}
+//	datatype element;
+//	printf("请输入入队的元素\n");
+//	scanf("%d", &element);
+//	q->data[q->rear] = element;
+//	q->rear = (q->rear + 1) % maxsize;
+//	q->tag = 1;
+//}
+//
+//datatype outqueue(queue* q)
+//{
+//	if (q->front == q->rear && q->tag == 0)
+//	{
+//		printf("is empty\n");
+//		exit(1);
+//	}
+//	datatype temp;
+//	temp = q->data[q->front];
+//	q->front = (q->front + 1) % maxsize;
+//	q->tag = 0;
+//	return temp;
+//}
+//
+//int main()
+//{
+//	queue q;
+//	initqueue(&q);
+//	bool flag = isempty(&q);
+//	if (!flag)
+//	{
+//		printf("error\n");
+//		return -1;
+//	}
+//	int i = 0;
+//	int n;
+//	printf("有几个元素要入队?\n");
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		enqueue(&q);
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", outqueue(&q));
+//	}
+//	return 0;
+//}
+
+
 typedef struct list
 {
 	datatype data;
@@ -22,7 +254,7 @@ void initqueue(queue* q)
 	q->front = q->rear = (list*)malloc(sizeof(list));
 	if (q->front == NULL)
 	{
-		printf("error\n");
+		printf("error");
 		return;
 	}
 	q->front->next = NULL;
@@ -39,7 +271,7 @@ bool isempty(queue* q)
 
 void enqueue(queue* q)
 {
-	printf("请输入元素的值\n");
+	printf("输入入队的元素\n");
 	datatype element;
 	scanf("%d", &element);
 	list* newnode = (list*)malloc(sizeof(list));
@@ -61,15 +293,16 @@ datatype outqueue(queue* q)
 		printf("empty\n");
 		exit(1);
 	}
-	datatype temp;
 	list* pointer;
+	datatype temp;
 	pointer = q->front->next;
 	temp = pointer->data;
-	q->front = pointer;
+	q->front->next = pointer->next;
 	if (pointer == q->rear)
 	{
 		q->rear = q->front;
 	}
+	free(pointer);
 	return temp;
 }
 
@@ -83,8 +316,8 @@ int main()
 		printf("error\n");
 		return -1;
 	}
-	printf("有几个元素要入队\n");
 	int n;
+	printf("有多少元素要入队\n");
 	scanf("%d", &n);
 	int i = 0;
 	for (i = 0; i < n; i++)
