a7201726c1122f69be774ab0618afe18383af23f
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-04-02 23:56:08 +0800

    Add files via upload
    2024.4.2链队

---
+++ 2024.4.2.exercise.c
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h> 
+
+typedef int datatype;
+
+typedef struct list
+{
+	datatype data;
+	struct list* next;
+}list;
+
+typedef struct queue
+{
+	list* front;
+	list* rear;
+}queue;
+
+void initqueue(queue* q)
+{
+	q->front = q->rear = (list*)malloc(sizeof(list));
+	if (q->front == NULL)
+	{
+		printf("error\n");
+		return;
+	}
+	q->front->next = NULL;
+}
+
+bool isempty(queue* q)
+{
+	if (q->front == q->rear && q->front->next == NULL)
+	{
+		return true;
+	}
+	return false;
+}
+
+void enqueue(queue* q)
+{
+	printf("输入元素的值\n");
+	datatype element;
+	scanf("%d", &element);
+	list* newnode = (list*)malloc(sizeof(list));
+	if (newnode == NULL)
+	{
+		printf("error\n");
+		return;
+	}
+	newnode->data = element;
+	newnode->next = NULL;
+	q->rear->next = newnode;
+	q->rear = newnode;
+}
+
+datatype outqueue(queue* q)
+{
+	if (isempty(q))
+	{
+		printf("error\n");
+		exit(1);
+	}
+	datatype temp;
+	list* pointer;
+	pointer = q->front->next;
+	temp = pointer->data;
+	q->front->next = pointer->next;
+	if (pointer == q->rear)
+	{
+		q->rear = q->front;
+	}
+	return temp;
+}
+
+int main()
+{
+	queue q;
+	initqueue(&q);
+	bool flag = isempty(&q);
+	if (!flag)
+	{
+		printf("error\n");
+		return -1;
+	}
+	int i = 0;
+	int n;
+	printf("有多少元素要入队?\n");
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		enqueue(&q);
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", outqueue(&q));
+	}
+	return 0;
+}
