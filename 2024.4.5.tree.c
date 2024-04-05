96da478029b3281dd973163deb9ec705e6b78217
AiShangYuWen <146498837+Zxlove720@users.noreply.github.com>
2024-04-05 15:16:51 +0800

    Add files via upload
    2024.4.5exercise

---
+++ 2024.4.5.tree.c
@@ -0,0 +1,178 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef int datatype;
+
+typedef struct tree
+{
+	datatype data;
+	struct tree* left;
+	struct tree* right;
+}tree;
+
+typedef struct list
+{
+	tree* data;
+	struct list* next;
+}list;
+
+typedef struct queue
+{
+	list* front;
+	list* rear;
+}queue;
+
+//tree* creatnode(tree* root)
+//{
+//	datatype element;
+//	scanf("%d", &element);
+//	if (element != 0)
+//	{
+//		root = (tree*)malloc(sizeof(tree));
+//		if (root == NULL)
+//		{
+//			printf("error\n");
+//			exit(1);
+//		}
+//		root->data = element;
+//		root->left = NULL;
+//		root->right = NULL;
+//		root->left = creatnode(root->left);
+//		root->right = creatnode(root->right);
+//	}
+//	return root;
+//}
+//
+//void visit(tree* root)
+//{
+//	printf("%d ", root->data);
+//}
+//
+//void preorder(tree* root)
+//{
+//	if (root != NULL)
+//	{
+//		visit(root);
+//		preorder(root->left);
+//		
+//		preorder(root->right);
+//	}
+//}
+//
+//int main()
+//{
+//	printf("请输入节点值\n");
+//	tree* root = NULL;
+//	root = creatnode(root);
+//	preorder(root);
+//	return 0;
+//}
+
+
+tree* creatnode(tree* root)
+{
+	datatype element;
+	scanf("%d", &element);
+	if (element != 0)
+	{
+		root = (tree*)malloc(sizeof(tree));
+		if (root == NULL)
+		{
+			printf("error\n");
+			exit(1);
+		}
+		root->data = element;
+		root->left = NULL;
+		root->right = NULL;
+		root->left = creatnode(root->left);
+		root->right = creatnode(root->right);
+	}
+	return root;
+}
+
+void initqueue(queue* q)
+{
+	q->front = q->rear = (list*)malloc(sizeof(list));
+	if (q->front == NULL)
+	{
+		printf("error\n");
+		return;
+	}
+	q->front->next = NULL;
+}
+
+bool isempty(queue* q)
+{
+	if (q->front == q->rear || q->front->next == NULL)
+	{
+		return true;
+	}
+	return false;
+}
+
+void enqueue(queue* q, tree* root)
+{
+	list* newnode = (list*)malloc(sizeof(list));
+	if (newnode == NULL)
+	{
+		printf("error\n");
+		return;
+	}
+	newnode->data = root;
+	newnode->next = NULL;
+	q->rear->next = newnode;
+	q->rear = newnode;
+}
+
+tree* outqueue(queue* q)
+{
+	list* pointer;
+	tree* temp;
+	pointer = q->front->next;
+	temp = pointer->data;
+	q->front->next = pointer->next;
+	if (q->front->next == NULL)
+	{
+		q->rear = q->front;
+	}
+	free(pointer);
+	pointer = NULL;
+	return temp;
+}
+
+void visit(tree* pointer)
+{
+	printf("%d ", pointer->data);
+}
+
+void preorder(tree* root)
+{
+	queue q;
+	initqueue(&q);
+	tree* pointer = root;
+	enqueue(&q, root);
+	while(!isempty(&q))
+	{
+		pointer = outqueue(&q);
+		visit(pointer);
+		if (pointer->left != NULL)
+		{
+			enqueue(&q, pointer->left);
+		}
+		if (pointer->right != NULL)
+		{
+			enqueue(&q, pointer->right);
+		}
+	}
+}
+
+int main()
+{
+	printf("请输入节点值\n");
+	tree* root = NULL;
+	root = creatnode(root);
+	preorder(root);
+	return 0;
+}
